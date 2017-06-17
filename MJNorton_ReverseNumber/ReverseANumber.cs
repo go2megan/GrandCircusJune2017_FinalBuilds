176a568e25826f37ca0f302e848a9acd0ab1fe6e
Megan J. Norton <go2megan@gmail.com>
2017-06-17 07:09:27 -0400

    MJNorton_ReverseNumber
    20170617:704a.m.
    Reverse Number Program
    References to learning how to write this are: https://www.youtube.com/watch?v=Q-mcQRA86B4

---
+++ MJNorton_ReverseNumber/ReverseANumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Projects.MJNorton_PreWork
+{
+    public class MJNReverseANumber
+    {
+        static void Main(string[] args)
+        {
+            int aNum, NumRev = 0;
+            Console.WriteLine("Enter a 4 digit number");
+            // the integer aNum assigns the value the user will enter, NumRev is zero a condition to stop the program
+            aNum = int.Parse(Console.ReadLine());
+            //insures that the entered value will convert from string to integer
+            while (aNum != 0)
+                //stops the program from searching for more numbers to parse and reverse
+            {//alorithm that moves say 1, 2, 3, 4 through 10's position and cycles through rotating the values to zero
+                NumRev = NumRev * 10;
+                NumRev = NumRev + aNum % 10;
+                aNum = aNum / 10;
+            }
+            Console.WriteLine("The reverse number is {0}", NumRev);
+            Console.ReadLine();
+            //pushes the message to interface as say 4321
+        }
+    }
+}
